dcae19e17dcba96ee86f5f3d3bd9d2425290e0f7
notKamui <teillard.jimmy@outlook.fr>
2024-12-02 08:29:51 +0100

    motion test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { motion, AnimatePresence } from "motion/react";
+import { useState } from "react";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      Hello "/"!
+      <button onClick={() => setShow((s) => !s)}>Toggle</button>
+      <AnimatePresence>
+        {show && (
+          <motion.p
+            key="text"
+            exit={{ x: 20, opacity: 0 }}
+            initial={{ x: -20, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+          >
+            Zooming in from left
+          </motion.p>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 }
